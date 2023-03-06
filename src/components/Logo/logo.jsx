97b3257a523aefc06ee67837ffaae00e76c9fc07
Elin Xristozov <73062722+ElinXr@users.noreply.github.com>
2023-03-06 17:30:31 +0200

    Update logo.jsx

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function Logo ({type='default'}) {
     let logo;
+        
     if (type==='default') {
-        logo = <img src='./images/logo.svg' alt="Default logo"/>;
-    } else if (type ==='muted') {
-        logo = <img src='./images/logo-muted.svg' alt="Muted logo"/>;
+            logo = <img src='./images/logo.svg' alt="Default logo"/>;
+     } else if (type ==='muted') {
+            logo = <img src='./images/logo-muted.svg' alt="Muted logo"/>;
     }
     return logo;
 };
