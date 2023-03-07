2cabf88a5823088757a7e106305468fd4ab849c6
Elin Xristozov <73062722+ElinXr@users.noreply.github.com>
2023-03-07 13:35:24 +0200

    Create User.jsx

---
+++ src/components/user/User.jsx
@@ -0,0 +1,17 @@
+import styles from "./User.module.scss";
+import Avatar from "../avatar/Avatar"
+
+export default function User ({ name = '', info = '', size = 50, avatar = '', verified = false }) {
+
+    return (
+    
+      <div className={styles.user}>
+         <Avatar url={avatar} size={size} verified={verified} />
+         
+        <div className={styles.infoContainer}>
+            <p className={styles.name}>{name}</p>
+            <p className={styles.info}>{info}</p>
+        </div>
+      </div>
+    );
+  };
